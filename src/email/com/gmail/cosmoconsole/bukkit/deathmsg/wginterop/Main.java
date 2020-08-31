aa78f80970916753ea598ff185a25f07bf94850e
CosmoConsole <cosmoconsole@gmail.com>
2020-08-31 20:19:24 +0300

    clean up indentation

--- src/email/com/gmail/cosmoconsole/bukkit/deathmsg/wginterop/Main.java
+++ src/email/com/gmail/cosmoconsole/bukkit/deathmsg/wginterop/Main.java
@@ -10,8 +10,8 @@ import email.com.gmail.cosmoconsole.bukkit.deathmsg.DeathMessageCustomEvent;
 import email.com.gmail.cosmoconsole.bukkit.deathmsg.DeathMessagesPrime;
 
 public class Main extends JavaPlugin implements Listener {
-	DeathMessagesPrime dmp;
-	WorldGuardInterface wg;
+    DeathMessagesPrime dmp;
+    WorldGuardInterface wg;
     
     @Override
     public void onLoad() {
@@ -19,30 +19,30 @@ public class Main extends JavaPlugin implements Listener {
         wg.register();
     }
     
-	public void onEnable() {
+    public void onEnable() {
         Bukkit.getPluginManager().registerEvents((Listener)this, (Plugin)this);
         dmp = (DeathMessagesPrime) Bukkit.getPluginManager().getPlugin("DeathMessagesPrime");
         wg.enable();
-	}
-	
-	@EventHandler
-	private void checkIfMessageShouldAppear(DeathMessageCustomEvent evt) {
-		if (evt.getPlayer() != null) {
-			if (evt.isPVP() && !wg.getBroadcastPVP(evt.getPlayer())) {
-				evt.setCancelled(true);
-			} else if (!evt.isPVP() && !wg.getBroadcastNatural(evt.getPlayer())) {
-				evt.setCancelled(true);
-			}
-		}
-	}
-	
-	@EventHandler
-	private void onDMPMessage(DeathMessageCustomEvent evt) {
-		if (evt.getPlayer() != null) {
-			String s = wg.getCustomMsg(evt.getPlayer());
-			if (s != null && !s.isEmpty()) {
-				evt.setTag("wgcustom." + s + "." + evt.getTag());
-			}
-		}
-	}
+    }
+    
+    @EventHandler
+    private void checkIfMessageShouldAppear(DeathMessageCustomEvent evt) {
+        if (evt.getPlayer() != null) {
+            if (evt.isPVP() && !wg.getBroadcastPVP(evt.getPlayer())) {
+                evt.setCancelled(true);
+            } else if (!evt.isPVP() && !wg.getBroadcastNatural(evt.getPlayer())) {
+                evt.setCancelled(true);
+            }
+        }
+    }
+    
+    @EventHandler
+    private void onDMPMessage(DeathMessageCustomEvent evt) {
+        if (evt.getPlayer() != null) {
+            String s = wg.getCustomMsg(evt.getPlayer());
+            if (s != null && !s.isEmpty()) {
+                evt.setTag("wgcustom." + s + "." + evt.getTag());
+            }
+        }
+    }
 }
